7f53cfe34e085c9a3b3ea38363b3dc2af9211ee9
Jon Ball <jonathon.ball@gmail.com>
2019-05-24 22:54:30 -0400

    experiment

--- index.php
+++ index.php
@@ -1,4 +1,101 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-echo "Hello world\n";
+    <title>jonathonball.com</title>
 
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+      html, body {
+          background-color: #fff;
+          font-family: 'Permanent Marker', cursive;
+          font-size: 48px;
+          height: 100vh;
+          margin: 0;
+      }
+
+      .full-height {
+          height: 100vh;
+      }
+
+      .flex-center {
+          align-items: center;
+          display: flex;
+          justify-content: center;
+      }
+
+      .position-ref {
+          position: relative;
+      }
+
+      .top-right {
+          position: absolute;
+          right: 10px;
+          top: 18px;
+          padding-right: 50px;
+      }
+
+      .content {
+          text-align: center;
+      }
+
+      .links > a {
+          color: #000;
+          padding: 20px;
+          text-decoration: none;
+      }
+
+      .logo {
+          color: #636b6f;
+          height: 64px;
+      }
+
+      .m-b-md {
+          margin-bottom: 30px;
+      }
+
+      .naked-list {
+          list-style: none;
+      }
+
+      .horizontal-list li {
+          display: inline;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="flex-center position-ref full-height">
+      <div class="top-right links">
+      </div>
+
+      <div class="content">
+        <div class="m-b-md">
+          Jonathon Ball <i>dot com</i>
+        </div>
+
+        <div class="links">
+          <a href="#">New Content Coming Soon</a>
+          <ul class="naked-list horizontal-list">
+            <li>
+              <a href="https://www.instagram.com/jonathonball/"><img class="logo" src="images/glyph-logo_May2016.png"/></a>
+            </li>
+            <li>
+              <a href="https://github.com/jonathonball"><img class="logo" src="images/GitHub-Mark-120px-plus.png"/></a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/vb.jon.ball"><img class="logo" src="images/f_logo_RGB-Black_72.png"/></a>
+            </li>
+            <li>
+              <a href="https://cornerofattention.com"><img class="logo" src="images/Tumblr_Logos_2018.03.06_tIconBlack.png"/></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
